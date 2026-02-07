961f037d0cc047da0814e5f198003ec33a621868
Mariam Sbeity <132481922+mariamsb03@users.noreply.github.com>
2026-02-08 00:41:52 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc'; 
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current directory
